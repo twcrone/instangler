7e7c54b76dc5dc133c36623ce5329d149107a583
Todd W Crone <twcrone@gmail.com>
2021-09-09 19:14:36 -0400

    Base test works

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,7 @@ fn main() {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(val) = line {
-                if val.contains("instrumentation") {
-                    let i = val.find(':');
-                    if i.is_some() {
-                        let j = i.unwrap();
-                        println!("{}", val.to_string().substring(j + 1, val.len() - 1));
-                    }
-                }
+                println!("{}", val)
             }
         }
     }
@@ -24,4 +18,36 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
     where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+fn extract_package(val: &str) -> &str {
+    if val.contains("instrumentation") {
+        let i = val.find(':');
+        if i.is_some() {
+            let j = i.unwrap();
+            //println!("{}", val.to_string().substring(j + 1, val.len() - 1));
+            let start = j + 1;
+            let end = val.len() - 1;
+            let pkg = val.substring(start, end);
+            return pkg
+        }
+    }
+    ""
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::extract_package;
+
+    #[test]
+    fn blank_is_blank() {
+        let actual = extract_package("");
+        assert_eq!(actual, "");
+    }
+
+    #[test]
+    fn it_works() {
+        //let actual = extract_package(&"include 'instrumentation:akka-2.2'");
+        //assert_eq!(actual, Some("akka-2.2"));
+    }
 }
