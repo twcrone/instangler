4b4c8eaf39cd9feee6c69cbf55fa0f875886f6c0
Todd W Crone <twcrone@gmail.com>
2021-09-09 17:09:14 -0400

    Initial read lines

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    // File hosts must exist in current path before this produces output
+    if let Ok(lines) = read_lines("settings.gradle") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(ip) = line {
+                println!("{}", ip);
+            }
+        }
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
